541996579995d21a70c782ff491697c9093c4141
Jusung Hwang (nabigraphics) <hbh2692@naver.com>
2024-08-18 01:55:21 +0900

    feat: 사용자 입력 처리

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,57 @@ export const App = () => {
     canvas.height = 600;
 
     let x = 0;
+    let y = 0;
+    const speed = 5;
+
+    const keys = {
+      left: false,
+      right: false,
+      up: false,
+      down: false,
+    };
+
+    const abort = new AbortController();
+
+    document.addEventListener(
+      "keydown",
+      (e) => {
+        if (e.key === "ArrowLeft") {
+          keys.left = true;
+        }
+        if (e.key === "ArrowRight") {
+          keys.right = true;
+        }
+        if (e.key === "ArrowUp") {
+          keys.up = true;
+        }
+        if (e.key === "ArrowDown") {
+          keys.down = true;
+        }
+      },
+      { signal: abort.signal }
+    );
+
+    document.addEventListener(
+      "keyup",
+      (e) => {
+        if (e.key === "ArrowLeft") {
+          keys.left = false;
+        }
+        if (e.key === "ArrowRight") {
+          keys.right = false;
+        }
+        if (e.key === "ArrowUp") {
+          keys.up = false;
+        }
+        if (e.key === "ArrowDown") {
+          keys.down = false;
+        }
+      },
+      {
+        signal: abort.signal,
+      }
+    );
 
     function loop() {
       requestAnimationFrame(loop);
@@ -25,18 +76,23 @@ export const App = () => {
       ctx.fillStyle = "#000";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+      // 키보드 입력에 따라 x, y 좌표 변경
+      if (keys.left) x -= speed;
+      if (keys.right) x += speed;
+      if (keys.up) y -= speed;
+      if (keys.down) y += speed;
+
       // 사각형 그리기
       ctx.fillStyle = "#f00";
-      ctx.fillRect(x, 100, 50, 50);
-
-      x += 2;
-
-      if (x > canvas.width) {
-        x = 0;
-      }
+      ctx.fillRect(x, y, 50, 50);
     }
 
     loop();
+
+    return () => {
+      // cleanup
+      abort.abort();
+    };
   }, []);
 
   return <canvas ref={canvasRef} className={styles.canvas} />;
