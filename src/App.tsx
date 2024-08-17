02f96f500d6437591299c6071e4eee88c7250216
Jusung Hwang (nabigraphics) <hbh2692@naver.com>
2024-08-18 01:49:09 +0900

    feat: 사각형 움직이기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
-import "./App.css";
+import { useEffect, useRef } from "react";
+
+import styles from "./App.module.css";
 
 export const App = () => {
-  return <canvas id="canvas" />;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    canvas.width = 800;
+    canvas.height = 600;
+
+    let x = 0;
+
+    function loop() {
+      requestAnimationFrame(loop);
+
+      // 캔버스 초기화 (배경을 검정색으로 채우기)
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // 사각형 그리기
+      ctx.fillStyle = "#f00";
+      ctx.fillRect(x, 100, 50, 50);
+
+      x += 2;
+
+      if (x > canvas.width) {
+        x = 0;
+      }
+    }
+
+    loop();
+  }, []);
+
+  return <canvas ref={canvasRef} className={styles.canvas} />;
 };
