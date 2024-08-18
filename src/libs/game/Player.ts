ac5eadda6d95ce4448ce063cbd67c6ed8682378d
Jusung Hwang (nabigraphics) <hbh2692@naver.com>
2024-08-18 16:15:13 +0900

    fix: Player 클래스 내부에서 입력 처리 하도록 변경

--- src/libs/game/Player.ts
+++ src/libs/game/Player.ts
@@ -1,31 +1,65 @@
 import { GameObject } from "./GameObject";
 
+class InputManager {
+  private keys: { [key: string]: boolean } = {};
+
+  constructor() {
+    window.addEventListener("keydown", this.onKeyDown.bind(this));
+    window.addEventListener("keyup", this.onKeyUp.bind(this));
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    this.keys[event.code] = true;
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.keys[event.code] = false;
+  }
+
+  isKeyDown(key: string) {
+    return this.keys[key] || false;
+  }
+
+  cleanup() {
+    window.removeEventListener("keydown", this.onKeyDown);
+    window.removeEventListener("keyup", this.onKeyUp);
+  }
+}
+
 export class Player extends GameObject {
+  // TODO: 생성자에서 InputManager를 주입 받을지 고민
+  private inputManager: InputManager = new InputManager();
+  private speed: number;
+
   constructor(
     x: number,
     y: number,
     width: number,
     height: number,
-    color: string
-  ) {
-    super(x, y, width, height, color);
-  }
-
-  update(
-    keys: { left: boolean; right: boolean; up: boolean; down: boolean },
+    color: string,
     speed: number
   ) {
-    if (keys.left) {
-      this.x -= speed;
+    super(x, y, width, height, color);
+
+    this.speed = speed;
+  }
+
+  update(deltaTime: number) {
+    if (this.inputManager.isKeyDown("ArrowLeft")) {
+      this.x -= this.speed;
     }
-    if (keys.right) {
-      this.x += speed;
+    if (this.inputManager.isKeyDown("ArrowRight")) {
+      this.x += this.speed;
     }
-    if (keys.up) {
-      this.y -= speed;
+    if (this.inputManager.isKeyDown("ArrowUp")) {
+      this.y -= this.speed;
     }
-    if (keys.down) {
-      this.y += speed;
+    if (this.inputManager.isKeyDown("ArrowDown")) {
+      this.y += this.speed;
     }
+  }
+
+  destory() {
+    this.inputManager.cleanup();
   }
 }
